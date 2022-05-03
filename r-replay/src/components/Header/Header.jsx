3b9c09a078f5a53deea16c6a5753bc73bda516a3
Aleksey-Smouzh <xmz@tut.by>
2022-05-03 16:47:26 +0200

    added menu

--- r-replay/src/components/Header/Header.jsx
+++ r-replay/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import header from "./Header.module.css";
+import Menu from './Menu/Menu';
 
 function Header() {
   return (
     <div className={header.container}>
       <h1>Wikipedia </h1>
+      <Menu></Menu>
     </div>
   )
 }
