b6b7fea06dc065ac3590b8a935e1acd9dcacecbe
Aleksey-Smouzh <xmz@tut.by>
2022-05-03 16:48:06 +0200

    comp Menu and style

---
+++ r-replay/src/components/Header/Menu/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import menubar from "./Menu.module.css"
+function Menu() {
+  return (
+    <div className={menubar.container}>
+        <nav >
+           <ul className={menubar.lists}>
+<li className={menubar.list}>1</li>
+<li className={menubar.list}>2</li>
+<li className={menubar.list}>3</li>
+<li className={menubar.list}>4</li>
+<li className={menubar.list}>5</li>
+<li className={menubar.list}>6</li>
+<li className={menubar.list}>7</li>
+<li className={menubar.list}>8</li>
+<li className={menubar.list}>9</li>
+<li className={menubar.list}>10</li>
+<li className={menubar.list}>11</li>
+<li className={menubar.list}>12</li>
+<li className={menubar.list}>13</li>
+<li className={menubar.list}>14</li>
+<li className={menubar.list}>15</li>
+           </ul>
+        </nav>
+        </div>
+  )
+}
+
+export default Menu
