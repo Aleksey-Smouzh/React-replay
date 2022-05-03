9d8cf7d292349986d028aa12b1cf576e513c3b17
Aleksey-Smouzh <xmz@tut.by>
2022-05-03 16:48:47 +0200

    fixed article, main, title,

--- r-replay/src/components/Main/Main.jsx
+++ r-replay/src/components/Main/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import main from "./Main.module.css"
 import article from "../../Data/articles.json"
-function Main() {
+function Main(props) {
     return (
         <div className={main.container}>
-            {article.map((item, index) =>
+            {article.map((item, index) => 
                 <section>
-                    <h2 key={index.id}>{item.title}</h2>
-                    <div key={index.id}>{item.body}</div>
+  <h2 key={index.id}>{item.title}</h2>
+   <div><p key={index.id}>{item.body}</p></div>
+  
+                   
                 </section>)}
         </div>
     )
---
+++ r-replay/src/components/Main/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Title(props) {
+  return (
+    <div>
+      
+       
+        </div>
+  )
+}
+
+export default Title
