20bd49ccc928699729eb67f11fc4b391a21d3f9f
Aleksey-Smouzh <xmz@tut.by>
2022-05-04 18:41:11 +0200

    unit3a myself

---
+++ r-replay/src/Unit3aapp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Unit3aapp() {
+  return (
+    <div>Unit3aapp</div>
+  )
+}
+
+export default Unit3aapp
--- r-replay/src/index.js
+++ r-replay/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import App3 from './App3';
+import Unit3aapp from './Unit3aapp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <App3/>
+    <Unit3aapp/>
   </React.StrictMode>
 );
 
